bda040d3c730fada567b21e4e64bec29a615e5c5
Alfredd1 <gnavarroalfred@gmail.com>
2025-08-22 22:24:02 -0400

    config insertion

--- callous-chroma/astro.config.mjs
+++ callous-chroma/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://alfredd1.github.io',
+    site: 'https://alfredd1/github.io',
     base: '/gbccsclubwebsite'
 });
